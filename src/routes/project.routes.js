2a3c67e48b02548247310bbf562793e3c3e076af
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2022-11-28 15:01:07 +0530

    fixes

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,11 +1,12 @@
+const path = require("path");
 const express = require('express');
 const router = express.Router();
 
 const project_controller = require('../controllers/project.controllers');
 
 router.get("/", (req, res) => {
-    console.log("B");
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+    console.log(path.join(__dirname, "../views", "index.html"));
+    res.sendFile(path.join(__dirname, "../views", "index.html"));
 });
 
 module.exports = router;
