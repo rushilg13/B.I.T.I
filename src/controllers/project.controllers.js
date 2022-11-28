5583aa8bde1a3ac898bed824f2092604ce8ca905
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2022-11-29 04:08:12 +0530

    Added Login Signup Routes

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -4,3 +4,23 @@ const Project = require('../models/project.models');
 exports.homepage = function (req, res) {
     res.sendFile(path.join(__dirname, "../views", "index.html"));
 }
+
+exports.business_signuppage = function (req, res) {
+    res.sendFile(path.join(__dirname, "../views", "business_signup.html"));
+}
+
+exports.business_home = function(req, res) {
+    res.send(req.body);
+}
+
+exports.business_loginpage = function (req, res) {
+    res.sendFile(path.join(__dirname, "../views", "business_login.html"));
+}
+
+exports.customer_signuppage = function (req, res) {
+    res.sendFile(path.join(__dirname, "../views", "customer_signup.html"));
+}
+
+exports.customer_loginpage = function (req, res) {
+    res.sendFile(path.join(__dirname, "../views", "customer_login.html"));
+}
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -6,4 +6,12 @@ const project_controller = require('../controllers/project.controllers');
 
 router.get("/", project_controller.homepage);
 
+router.get("/business_signup", project_controller.business_signuppage);
+router.post("/business_home", project_controller.business_home);
+
+
+router.get("/business_login", project_controller.business_loginpage);
+router.get("/customer_signup", project_controller.customer_signuppage);
+router.get("/customer_login", project_controller.customer_loginpage);
+
 module.exports = router;
