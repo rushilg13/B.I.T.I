1c85330dba34dcf3302329d0dea50baeb7487295
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2022-12-01 01:53:02 +0530

    Configured Business signup signin

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,5 +1,7 @@
 const path = require("path");
-const Project = require('../models/project.models');
+const customer_db = require('../models/project.customer');
+const order_db = require('../models/project.order');
+const shop_db = require('../models/project.shop');
 
 exports.homepage = function (req, res) {
     res.sendFile(path.join(__dirname, "../views", "index.html"));
@@ -9,8 +11,70 @@ exports.business_signuppage = function (req, res) {
     res.sendFile(path.join(__dirname, "../views", "business_signup.html"));
 }
 
-exports.business_home = function(req, res) {
-    res.send(req.body);
+exports.business_home = function (req, res) {
+    res.send("home");
+}
+
+exports.business_home_signup = function (req, res) {
+    let business = new shop_db({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        address: req.body.address,
+        password: req.body.password,
+        categories: req.body.categories // fix me
+    });
+
+    shop_db.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
+            //handle error here
+            console.error(err);
+        }
+
+        //if a user was found, that means the user's email matches the entered email
+        if (user) {
+            var err = new Error('A business with this email has already registered. Please login.')
+            err.status = 400;
+            res.redirect('/business_login');
+            return err;
+        } else {
+            //code if no user with entered email was found
+            business.save(function (err) {
+                if (err) {
+                    return (err);
+                }
+                res.redirect('/business_home')
+            })
+        }
+    });
+}
+
+exports.business_home_login = function (req, res) {
+    shop_db.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
+            //handle error here
+            console.error(err);
+        }
+
+        //if a user was found, that means the user's email matches the entered email
+        if (user) {
+            if (user.password === req.body.password) {
+                res.redirect('/business_home')
+            }
+            else {
+                var err = new Error('Incorrect Password.')
+                err.status = 400;
+                res.redirect('/business_login');
+                return err;
+            }
+        } else {
+            //code if no user with entered email was found
+            var err = new Error('Invalid email ID. Please signup.')
+            err.status = 400;
+            res.redirect('/business_signup');
+            return err;
+        }
+    });
 }
 
 exports.business_loginpage = function (req, res) {
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,17 +1,52 @@
 const path = require("path");
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const project_controller = require('../controllers/project.controllers');
 
 router.get("/", project_controller.homepage);
 
 router.get("/business_signup", project_controller.business_signuppage);
-router.post("/business_home", project_controller.business_home);
+router.post("/business_home_signup", project_controller.business_home_signup);
+router.post("/business_home_login", project_controller.business_home_login);
+router.get("/business_home", project_controller.business_home);
+
+
+router.get('/google', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}));
+
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: "/",
+}), function (req, response) {
+    req.session.userData = {
+        profile: req.user.photos[0].value,
+        email: req.user.email
+    };
+    req.session.loggedin = true;;
+
+    response.redirect("/business_home");
+    response.end();
+});
 
 
 router.get("/business_login", project_controller.business_loginpage);
 router.get("/customer_signup", project_controller.customer_signuppage);
 router.get("/customer_login", project_controller.customer_loginpage);
+
+router.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.status(400).send('Unable to log out')
+            } else {
+                res.send('Logout successful')
+            }
+        });
+    } else {
+        res.end()
+    }
+})
 
 module.exports = router;
