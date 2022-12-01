26de867ae205617da9f1f716dd8a5f793dd48ce5
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2022-12-01 15:40:29 +0530

    Added Create Order - Modal

--- src/models/project.order.js
+++ src/models/project.order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
+    orderDesc: String,
     orderID: mongoose.Schema.Types.ObjectId,
     dateOfOrder: Date,
     dueDate: Date,
